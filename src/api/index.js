967dc6a9fdeb16ce960e3a913122c26637aa75a8
Danila Pisarev <jobdan@bk.ru>
2022-01-07 21:31:36 +0300

    Class to work with api is completed

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import qs from "query-string";
-const DOMAIN = "http://localhost:3001/";
+export const DOMAIN = "http://localhost:3001/";
 
-class AppCall {
+// Class to wort with api
+class ApiCall {
   constructor(domain) {
     this.domain = domain;
   }
@@ -21,7 +22,21 @@ class AppCall {
   async get(path, searchParams = {}) {
     return await this.perform(`${path}?${qs.stringify(searchParams)}`);
   }
-  async post() {}
-  async put() {}
-  async delete() {}
+  async post(path, payload) {
+    return await this.perform(path, payload, {
+      method: "POST",
+    });
+  }
+  async put(path, payload) {
+    return await this.perform(path, payload, {
+      method: "PUT",
+    });
+  }
+  async delete(path) {
+    return await this.perform(path, {
+      method: "PUT",
+    });
+  }
 }
+
+export default new ApiCall(DOMAIN);
